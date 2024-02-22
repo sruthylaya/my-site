4ef202687c4a8c5510d9e431c7fc6be9a8067817
sruthylaya <135493115+sruthylaya@users.noreply.github.com>
2024-02-22 16:35:35 +0530

    add

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,38 +1,27 @@
 from datetime import date
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 all_posts = [
-    {
-        "slug": "hike-in-the-mountain",
-        "image": "mountains.jpg",
-        "author": "Maxmillian",
-        "date": date(2021, 7, 22),
-        "title": "Mountain hiking",
-        "except": "There is nothing",
-        "content": """Lorem ipsum doloar"""
-
-    }
+    
 ]
 
-def get_date(post):
-    return post['date']
-
-
-# Create your views here.
-
-
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", {
         "posts": latest_posts
     })
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html",{
         "all_posts":all_posts
     })
 
 
 def post_detail(request, slug):
-    return render(request, "blog/post-detail.html")
+    identified_post = get_object_or_404(Post, slug=slug)
+    return render(request, "blog/post-detail.html", {
+        "post": identified_post,
+        "post_tags": identified_post.tag.all()
+    })
